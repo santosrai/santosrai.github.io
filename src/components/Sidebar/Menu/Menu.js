20397fafec2f90f72589edc384672111de7699cf
santosrai <me.santosrai@gmail.com>
2021-01-02 12:26:22 +0900

    The 'Newsletter' link of Menu gives a 404 error Fixes #8

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -15,13 +15,22 @@ const Menu = ({ menu }: Props) => (
     <ul className={styles['menu__list']}>
       {menu.map((item) => (
         <li className={styles['menu__list-item']} key={item.path}>
-          <Link
-            to={item.path}
-            className={styles['menu__list-item-link']}
-            activeClassName={styles['menu__list-item-link--active']}
-          >
-            {item.label}
-          </Link>
+          {item.label === 'Newsletter'
+            ? <a
+              className={styles['menu__list-item-link']}
+              href={item.path}
+              activeClassName={styles['menu__list-item-link--active']}
+              target={'_blank'}
+            >
+              {item.label}
+            </a>
+            : <Link
+              to={item.path}
+              className={styles['menu__list-item-link']}
+              activeClassName={styles['menu__list-item-link--active']}
+            >
+              {item.label}
+            </Link>}
         </li>
       ))}
     </ul>
