9fe4e113eab003901e34d1211704ab4418edeb90
santosrai <me.santosrai@gmail.com>
2021-03-09 23:26:08 +0900

    initial

---
+++ src/components/Projects/index.js
@@ -0,0 +1,7 @@
+// @flow strict
+export { default } from './Project';
+
+// TODO: Make Project portfolio
+// Same as Pages Strategy
+//    - create markdown file per project with description on project folder
+//    - render the file and display grid on project page
