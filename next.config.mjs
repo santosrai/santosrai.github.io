7cb3b35d2879fd33e5e442bfd9297a42253e8e94
Santosh Rai <me.santosrai@gmail.com>
2025-08-21 11:24:21 -0700

    Completely disable SWC in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Completely disable SWC
   swcMinify: false,
   experimental: {
     forceSwcTransforms: false,
+    swcTraceProfiling: false,
   },
-  compiler: {
-    // Disable SWC and use Babel instead
-    removeConsole: false,
+  // Use Terser for minification instead of SWC
+  webpack: (config, { dev, isServer }) => {
+    // Disable SWC loader
+    if (!dev && !isServer) {
+      config.optimization.minimizer = config.optimization.minimizer.filter(
+        (minimizer) => minimizer.constructor.name !== 'SwcMinifyPlugin'
+      );
+    }
+    return config;
   },
 };
 
